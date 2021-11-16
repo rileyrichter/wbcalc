2e83ac651c58a2d209f5437979c95b4a59ea9608
Ben Parker <bparker@gmail.com>
2021-11-15 21:19:42 -0600

    🐛 FIX: Crush that bug!

--- calc.js
+++ calc.js
@@ -88,7 +88,7 @@ form.addEventListener("click", function (evt) {
       document.getElementById("scaleRS").textContent = scaleRS;
       let hoursFC = Number(data[0].fields.hoursFC);
       let hoursFC2 = "$" + hoursFC.toFixed(4);
-      let hoursRS = "$" + Number(data[0].fields.hoursRS).toFixed(2);
+      let hoursRS = "$" + Number(data[0].fields.hourRS).toFixed(2);
       document.getElementById("hoursFC").textContent = hoursFC2;
       document.getElementById("hoursRS").textContent = hoursRS;
       let wrapbookfee = Number(data[0].fields.wrapbookfee) * 100;
