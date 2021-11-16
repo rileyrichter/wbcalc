2cd3ea3ddc85e6450982fef0c129a8cec1f2433f
Ben Parker <bparker@gmail.com>
2021-11-15 20:06:42 -0600

    Initial commit

---
+++ calc.js
@@ -0,0 +1,51 @@
+// Gets a reference to the form element
+const form = document.getElementById("form-submit");
+
+// Adds a listener for the "submit" event.
+form.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  const productionState = document.getElementById("production-state").value;
+  const unionValue = document.getElementById("union").value;
+  const contractValue = document.getElementById("contract").value;
+  const wagesValue = Number(document.getElementById("wages").value);
+  const hoursValue = Number(document.getElementById("hours").value);
+  const scaleValue = Number(document.getElementById("scale").value);
+
+  const handleError = (response) => {
+    if (!response.ok) {
+      throw Error(` ${response.status} ${response.statusText}`);
+    } else {
+      return response.json();
+    }
+  }; //handler function that throws any encountered error
+
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  let requestOptions = {
+    method: "post",
+    headers: myHeaders,
+    redirect: "follow",
+    body: JSON.stringify([
+      {
+        taxes: [productionState],
+        wages: wagesValue,
+        contracts: [contractValue],
+        hours: hoursValue,
+        scalerate: scaleValue,
+        wrapbookfee: 0.0149,
+      },
+    ]),
+  };
+
+  fetch(
+    "https://v1.nocodeapi.com/rileyrichter/airtable/ciKZoDbzdFgWoHYz?tableName=Requests",
+    requestOptions
+  )
+    .then(handleError)
+    .then((data) => {
+      let socialsecurityFS = Number(data[0].fields.socialsecurityFS) * 100;
+      let socialsecurityFS2 = socialsecurityFS.toFixed(2) + "%";
+      console.log(socialsecurityFS2);
+      console.log(data);
+    });
+});
