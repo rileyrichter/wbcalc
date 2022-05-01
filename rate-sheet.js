4788685b4a6c0beb46c3c29357c32da0f471f73f
Ben Parker <bparker@gmail.com>
2022-05-01 14:48:43 -0500

    Update columns for rate sheet

--- rate-sheet.js
+++ rate-sheet.js
@@ -23,7 +23,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     .then(handleError)
     .then((data) => {
       data.records.forEach((record) => {
-        // do something here
         let newRow = dataRow.cloneNode(true);
         newRow.id = record.id;
         let jobTitle = newRow.getElementsByClassName("position")[0];
@@ -40,36 +39,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
         } else {
           hourlyRate.innerText = `\u2014`;
         }
-        let eightHours = newRow.getElementsByClassName("eight-hours")[0];
-        if (record.fields.eight_hours == null) {
-          eightHours.innerText = `\u2014`;
-        } else {
-          eightHours.innerText = `$ ${record.fields.eight_hours.toFixed(2)}`;
-        }
-        let tenHours = newRow.getElementsByClassName("ten-hours")[0];
-        if (record.fields.ten_hours == null) {
-          tenHours.innerText = `\u2014`;
-        } else {
-          tenHours.innerText = `$ ${record.fields.ten_hours.toFixed(2)}`;
-        }
         let twelveHours = newRow.getElementsByClassName("twelvel-hours")[0];
         if (record.fields.twelve_hours == null) {
           twelveHours.innerText = `\u2014`;
         } else {
           twelveHours.innerText = `$ ${record.fields.twelve_hours.toFixed(2)}`;
         }
-        let dayRate = newRow.getElementsByClassName("day-rate")[0];
-        dayRate.innerText = `$213.52`;
         let fringeRate = newRow.getElementsByClassName("fringe-rate")[0];
         fringeRate.innerText = `44.50%`;
         let total = newRow.getElementsByClassName("total")[0];
         total.innerText = `$123.45`;
-        let filter = record.fields.filter;
-        let budget = record.fields.budget;
-        let union = record.fields.union;
-        let region = record.fields.region;
-        let guaranteedHours = record.fields.guaranteed_hours;
-        let validUntil = record.fields.valid_until;
         root.appendChild(newRow);
       });
     })
